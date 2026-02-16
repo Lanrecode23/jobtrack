02399c97a39f2a89a0444c00787b211bd1f37992
Lanrecode23 <lanrecode23@gmail.com>
2026-02-16 15:50:09 +0100

    add

--- jobtracker/src/Home/Home.jsx
+++ jobtracker/src/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Features from '../components/Features'
-import CTA from '../components/Cta'
+import CTA from '../components/CTA'
 import Footer from '../components/Footer'
 
 function Home() {
